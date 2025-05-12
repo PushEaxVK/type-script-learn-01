2b2e27a0055ea93b89c5a73d1dc6156c067c2d45
PushEax <pusheax.vk@gmail.com>
2025-05-13 01:59:33 +0300

    enum

--- src/types-for-vars-and-args.ts
+++ src/types-for-vars-and-args.ts
@@ -44,4 +44,56 @@ const date: [number, number, number] = [1, 10, 2004];
 
 const tuple: [string, ...number[]] = ['hello', 1, 2, 3, 4, 5];
 
-use(num, data, tupleType, date, tuple);
+// Enum
+enum Role {
+  ADMIN,
+  USER
+}
+
+const person = {
+  role: Role.ADMIN
+};
+
+// Як літерал
+type Role2 = 'ADMIN' | 'USER';
+
+const person2: { role: Role2 } = {
+  role: 'ADMIN'
+};
+
+// const об'єкт + typeof
+const Role3 = {
+  ADMIN: 0,
+  USER: 1
+} as const;
+
+type Role3 = (typeof Role3)[keyof typeof Role3];
+
+const person3 = {
+  role: Role3.ADMIN
+};
+
+console.log(Role.ADMIN);
+console.log(Role[Role.ADMIN]);
+
+// rename
+enum UserStatus {
+  Active = 'ACTIVE',
+  Inactive = 'INACTIVE',
+  Banned = 'Banned'
+}
+
+const status: UserStatus = UserStatus.Active;
+
+// const enum - replaced by literals
+// їх не можна використовувати у виразах, які вимагають виконання під час виконання
+const enum HttpCodes {
+  OK = 200,
+  BadRequest = 400,
+  Unauthorized = 401
+}
+
+const status2 = HttpCodes.OK;
+console.log(status2);
+
+use(num, data, tupleType, date, tuple, person, person2, person3, status);

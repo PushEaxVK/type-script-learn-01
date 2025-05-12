257bcb55403beea9cffe3822ac26a0f8660986e9
PushEax <pusheax.vk@gmail.com>
2025-05-13 01:18:07 +0300

    any

---
+++ src/types-for-vars-and-args.ts
@@ -0,0 +1,31 @@
+import { use } from './base-types';
+
+// any - варто уникати використання
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let notSure: any = 4;
+notSure = 'maybe a string instead';
+notSure = false;
+notSure = {};
+
+const num: number = notSure;
+
+use(num);
+
+function fetchData() {
+  // some fetch
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const data: any = fetchData();
+use(data);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function fetchUserData(id: string, callback: (data: any) => void): void {
+  const responseData = {
+    name: 'Tom',
+    id
+  };
+  callback(responseData);
+}
+
+fetchUserData('123', use);

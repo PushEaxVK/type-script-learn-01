062f1b348dc4e5972a548aa24f33d2caf794bcef
PushEax <pusheax.vk@gmail.com>
2025-05-13 01:23:34 +0300

    tuple

--- src/types-for-vars-and-args.ts
+++ src/types-for-vars-and-args.ts
@@ -9,15 +9,12 @@ notSure = {};
 
 const num: number = notSure;
 
-use(num);
-
 function fetchData() {
   // some fetch
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const data: any = fetchData();
-use(data);
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function fetchUserData(id: string, callback: (data: any) => void): void {
@@ -39,3 +36,12 @@ const userData: unknown = fetchSomeData();
 if (typeof userData === 'string') {
   console.log(userData.toUpperCase());
 }
+
+// tuple
+const tupleType: [string, boolean] = ['Hello', true];
+
+const date: [number, number, number] = [1, 10, 2004];
+
+const tuple: [string, ...number[]] = ['hello', 1, 2, 3, 4, 5];
+
+use(num, data, tupleType, date, tuple);
